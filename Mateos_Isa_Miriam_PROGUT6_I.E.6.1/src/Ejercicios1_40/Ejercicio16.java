0caddd44412593edf3db569dac45d51cf8fe5244
Miriam <Miriam@Miriam>
2020-01-07 17:10:18 +0100

    07/01/2020 17.10

---
+++ Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio16.java
@@ -0,0 +1,31 @@
+package Ejercicios1_40;
+
+public class Ejercicio16 {
+
+	public static void main(String[] args) {
+
+		int array[] = new int[20];
+		
+		int n=1;
+		for(int i=0; i<array.length; i++) {
+			array[i]=n;
+			n++;
+			System.out.println(array[i]);
+		}
+		
+		int suma=0;
+		int mayor=array[0];
+		for(int i=0; i<array.length; i++) {
+			if(i%2==0) {
+				suma+=array[i];
+			}else {
+				if(array[i]>mayor) {
+					mayor=array[i];
+				}
+			}
+		}
+		System.out.println("La suma de los numeros en posiciones pares es de: "+suma);
+		System.out.println("El mayor de las posicion de los inpares es: "+mayor);
+	}
+
+}
---
+++ Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio17.java
@@ -0,0 +1,10 @@
+package Ejercicios1_40;
+
+public class Ejercicio17 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
