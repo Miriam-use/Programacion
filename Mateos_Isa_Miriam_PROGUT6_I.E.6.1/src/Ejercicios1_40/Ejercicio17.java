7841339348bb05bbc7220b9412d5695a7665bdf3
Miriam <Miriam@Miriam>
2020-01-08 18:07:55 +0100

    08/01/2020	18.07

--- Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio17.java
+++ Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio17.java
@@ -4,5 +4,23 @@ public class Ejercicio17 {
 
 	public static void main(String[] args) {
 
+		int cnp = 0;
+        int num[] = new int[10];
+        
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (int) (Math.random() * 100);
+            int res = 0;
+            for (int j = 1; j <= num[i]; j++) {
+                if (num[i] % j == 0) {
+                    res++;
+                }
+            }
+            if (res == 2) {
+                cnp++;
+                System.out.println("numero primo = " + num[i]);
+            }
+ 
+        }
+        System.out.println("La cantidad de numeros primos es: " + cnp);
 	}
 }
--- Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio23.java
+++ Mateos_Isa_Miriam_PROGUT6_I.E.6.1/src/Ejercicios1_40/Ejercicio23.java
@@ -1,7 +1,5 @@
 package Ejercicios1_40;
 
-import java.util.Scanner;
-
 public class Ejercicio23 {
 
 	public static void main(String[] args) {
